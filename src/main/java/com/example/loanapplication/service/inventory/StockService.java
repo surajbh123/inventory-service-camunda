097bb3e3325e6253b187f971909becf0669c2fce
suraj bhadrike <56318561+surajbh123@users.noreply.github.com>
2026-03-14 01:11:27 +0530

    refactor code

--- src/main/java/com/example/loanapplication/service/inventory/StockService.java
+++ src/main/java/com/example/loanapplication/service/inventory/StockService.java
@@ -1,4 +1,10 @@
 package com.example.loanapplication.service.inventory;
 
+import org.jvnet.hk2.annotations.Service;
+
+@Service
 public class StockService {
+
+//    private final InventoryJdbcRepository inventoryJdbcRepository;
+
 }
